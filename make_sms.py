deccf77b1266c5f50878e7aa07585c8860fbaa68
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2024-07-13 15:42:00 +0900

    Add files via upload

---
+++ make_sms.py
@@ -0,0 +1,10 @@
+# 한글 문자열
+hangul_string = "도움이 필요합니다."
+
+# 유니코드로 변환
+unicode_string = ""
+for char in hangul_string:
+    unicode_string +=format(ord(char), "04x")
+
+print("한글 문자열:", hangul_string)
+print("유니코드:", unicode_string.upper())
